efb2de084da6807c3b5fee5587bf461923009f80
NT <neoanderson9x@gmail.com>
2019-10-23 21:04:16 +0700

    task02

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get  'static_pages/home'
-  get  'static_pages/help'
+  get  '/help',  to: 'static_pages#help'
+  get  '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  resources :users
 end
